bd899f0c2477b1fbdbb63220f962c54e49e3842e
Krutyi 4el <60041069+Krutyi-4el@users.noreply.github.com>
2023-05-23 21:51:29 +0300

    update user agent

--- musicbot/linkutils.py
+++ musicbot/linkutils.py
@@ -35,7 +35,7 @@ headers = {
         (
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
             "AppleWebKit/537.36 (KHTML, like Gecko)",
-            "Chrome/96.0.4664.110",
+            "Chrome/113.0.5672.126",
             "Safari/537.36",
         )
     )
