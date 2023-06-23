2b28193e9dcc142192a205db50bb467a46fac39e
Krutyi 4el <60041069+Krutyi-4el@users.noreply.github.com>
2023-06-23 19:07:44 +0300

    fully switch to spawn context

--- musicbot/loader.py
+++ musicbot/loader.py
@@ -2,6 +2,7 @@ import sys
 import asyncio
 import threading
 from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import get_context as mp_context
 from typing import List, Tuple, Optional, Union
 
 import yt_dlp
@@ -16,7 +17,7 @@ sys.stdout = OutputWrapper(sys.stdout)
 sys.stderr = OutputWrapper(sys.stderr)
 
 _loop = asyncio.new_event_loop()
-_executor = ProcessPoolExecutor(1)
+_executor = ProcessPoolExecutor(1, mp_context("spawn"))
 _cached_downloaders: List[Tuple[dict, yt_dlp.YoutubeDL]] = []
 _preloading = {}
 _search_lock = threading.Lock()
