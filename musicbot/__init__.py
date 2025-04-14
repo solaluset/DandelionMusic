1628084d06f5477e654442b246d5ca163bacb1a7
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2025-04-14 11:26:50 +0300

    Try to fix plugins again

--- musicbot/__init__.py
+++ musicbot/__init__.py
@@ -1,4 +1,10 @@
 __all__ = ("loader",)
 
+import os
+import sys
+
+# to load yt-dlp plugin
+sys.path.append(os.path.dirname(__file__))
+
 # avoid circular import error
-from . import loader
+from . import loader  # noqa: E402
