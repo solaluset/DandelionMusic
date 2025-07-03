49596b70f959295c5d3eb10e6f45023c7a68674c
solaluset <60041069+solaluset@users.noreply.github.com>
2025-07-04 01:35:45 +0300

    Fix yt-dlp pligins not loading

--- musicbot/__init__.py
+++ musicbot/__init__.py
@@ -1,4 +1,13 @@
+import os
+import sys
+
+from yt_dlp.plugins import load_all_plugins
+
 __all__ = ("loader",)
 
+# load yt-dlp plugins
+sys.path.append(os.path.dirname(__file__))
+load_all_plugins()
+
 # avoid circular import error
-from . import loader
+from . import loader  # noqa: E402
