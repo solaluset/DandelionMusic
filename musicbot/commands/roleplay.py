9c8556eff63cb0c6e37d3abe6aed6aaa0fe5d525
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2025-08-30 16:51:05 +0300

    Add more RP commands

--- musicbot/commands/roleplay.py
+++ musicbot/commands/roleplay.py
@@ -53,21 +53,22 @@ class RolePlay(commands.Cog):
         CommandInfo("clap", "Аплодувати", "clap", "{} аплодує", False),
         CommandInfo(
             "confused",
-            "Бути розгубленим",
+            "Розгубитися",
             "confused",
             "{} почувається розгублено",
             False,
         ),
         CommandInfo(
-            "cool", "Бути крутим", "cool", "{} почувається круто", False
+            "cool", "Почуватися круто", "cool", "{} почувається круто", False
         ),
         CommandInfo("cry", "Плакати", "cry", "{} плаче", False),
         CommandInfo(
             "cuddle", "Міцно обійняти", "cuddle", "{} міцно обіймає {}"
         ),
         CommandInfo("dance", "Танцювати", "dance", "{} танцює", False),
-        CommandInfo("hug", "Обійняти користувача", "hug", "{} обіймає {}"),
-        CommandInfo("kiss", "Поцілувати користувача", "kiss", "{} цілує {}"),
+        CommandInfo(
+            "drool", "Пускати слину", "drool", "{} пускає слину", False
+        ),
         CommandInfo(
             "evil-laugh",
             "Зловісно сміятися",
@@ -75,8 +76,125 @@ class RolePlay(commands.Cog):
             "{} зловісно сміється",
             False,
         ),
+        CommandInfo(
+            "facepalm", "Фейспалм", "facepalm", "{} робить фейспалм", False
+        ),
+        CommandInfo(
+            "handhold", "Триматися за руки", "handhold", "{} тримає {} за руку"
+        ),
+        CommandInfo(
+            "happy",
+            "Почуватися щасливо",
+            "happy",
+            "{} почувається щасливо",
+            False,
+        ),
+        CommandInfo(
+            "headbang",
+            "Битися головою об стіну",
+            "headbang",
+            "{} б'ється головою об стіну",
+            False,
+        ),
+        CommandInfo("hug", "Обійняти користувача", "hug", "{} обіймає {}"),
+        CommandInfo("huh", "Га?", "huh", "{} розгублено дивиться", False),
+        CommandInfo("kiss", "Поцілувати користувача", "kiss", "{} цілує {}"),
+        CommandInfo("laugh", "Сміятися", "laugh", "{} сміється", False),
+        CommandInfo("lick", "Лизати користувача", "lick", "{} лиже {}"),
+        CommandInfo(
+            "love", "Відчувати любов", "love", "{} випромінює любов", False
+        ),
+        CommandInfo("mad", "Злитися", "mad", "{} злиться", False),
+        CommandInfo("nervous", "Нервувати", "nervous", "{} нервує", False),
+        CommandInfo("no", "Заперечувати", "no", "{} заперечує", False),
+        CommandInfo("nom", "Їсти", "nom", "{} їсть", False),
+        CommandInfo(
+            "nosebleed",
+            "Кров з носа",
+            "nosebleed",
+            "У {} йде кров з носа",
+            False,
+        ),
+        CommandInfo("nuzzle", "Тертися", "nuzzle", "{} треться об {}"),
+        CommandInfo(
+            "nyah",
+            "Робити милий вигляд",
+            "nyah",
+            "{} має милий вигляд",
+            False,
+        ),
+        CommandInfo("pat", "Гладити", "pat", "{} гладить {}"),
+        CommandInfo("peek", "Підглядати", "peek", "{} підглядає за {}"),
+        CommandInfo("pinch", "Ущипнути", "pinch", "{} щипає {}"),
+        CommandInfo("poke", "Тицьнути", "poke", "{} тицяє {}"),
+        CommandInfo("pout", "Дутися", "pout", "{} дується", False),
+        CommandInfo("punch", "Вдарити", "punch", "{} б'є {}"),
+        CommandInfo("roll", "Крутитися", "roll", "{} крутиться", False),
+        CommandInfo("run", "Бігти", "run", "{} біжить", False),
+        CommandInfo("sad", "Сумувати", "sad", "{} сумує", False),
+        CommandInfo("scared", "Злякатися", "scared", "{} боїться", False),
+        CommandInfo("shout", "Кричати", "shout", "{} кричить", False),
+        CommandInfo(
+            "shrug", "Знизати плечима", "shrug", "{} знизує плечима", False
+        ),
+        CommandInfo("shy", "Соромитися", "shy", "{} соромиться", False),
+        CommandInfo("sigh", "Зітхати", "sigh", "{} зітхає", False),
+        CommandInfo("sip", "Зробити ковток", "sip", "{} п'є", False),
+        CommandInfo("slap", "Дати ляпаса", "slap", "{} дає ляпаса {}"),
+        CommandInfo("sleep", "Спати", "sleep", "{} спить", False),
+        CommandInfo(
+            "slow-clap",
+            "Повільно аплодувати",
+            "slowclap",
+            "{} повільно аплодує",
+            False,
+        ),
+        CommandInfo(
+            "smack", "Вдарити по голові", "smack", "{} б'є {} по голові"
+        ),
+        CommandInfo("smile", "Посміхатися", "smile", "{} посміхається", False),
+        CommandInfo(
+            "smug",
+            "Почуватися самовдоволено",
+            "smug",
+            "{} почувається самовдоволено",
+            False,
+        ),
+        CommandInfo("sneeze", "Чхнути", "sneeze", "{} чхає", False),
+        CommandInfo(
+            "sorry", "Просити вибачення", "sorry", "{} просить вибачення у {}"
+        ),
+        CommandInfo("stare", "Дивитися", "stare", "{} дивиться на {}"),
+        CommandInfo("rp-stop", "Зупиняти когось", "stop", "{} зупиняє {}"),
+        CommandInfo(
+            "surprised", "Дивуватися", "surprised", "{} дивується", False
+        ),
+        CommandInfo("sweat", "Пітніти", "sweat", "{} пітніє", False),
+        CommandInfo(
+            "thumbsup",
+            "Палець вгору",
+            "thumbsup",
+            "{} підіймає великий палець вгору",
+            False,
+        ),
+        CommandInfo("tickle", "Лоскотати", "tickle", "{} лоскоче {}"),
+        CommandInfo(
+            "tired",
+            "Почуватися втомлено",
+            "tired",
+            "{} почувається втомлено",
+            False,
+        ),
+        CommandInfo("wave", "Махати руками", "wave", "{} махає руками до {}"),
+        CommandInfo("wink", "Підморгувати", "wink", "{} підморгує {}"),
+        CommandInfo("woah", "Захоплюватися", "woah", "{} захоплюється", False),
+        CommandInfo("yawn", "Позіхати", "yawn", "{} позіхає", False),
+        CommandInfo("yay", "Радіти", "yay", "{} радіє", False),
+        CommandInfo("yes", "Погоджуватися", "yes", "{} погоджується", False),
     ]
     COMMANDS = {info.name: info for info in COMMANDS}
+    # a stupid discord limit - no more than 5 user commands
+    USER_COMMANDS = {"hug", "kiss", "bite", "handhold", "pat"}
 
     def __init__(self, bot: MusicBot):
         self.bot = bot
@@ -93,7 +211,8 @@ class RolePlay(commands.Cog):
             }
             if info.needs_other_user:
                 bot.slash_command(**args)(self._callback_with_user_arg)
-                bot.user_command(**args)(self._callback_with_user_arg)
+                if info.name in self.USER_COMMANDS:
+                    bot.user_command(**args)(self._callback_with_user_arg)
             else:
                 bot.slash_command(**args)(self._callback_without_args)
 
