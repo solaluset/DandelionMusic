d73af093f8dbb114919fcc6e2780078909ebb439
Sóla Łuset <60041069+solaluset@users.noreply.github.com>
2026-05-10 13:31:29 +0300

    Add repost support to Discord extractor

--- musicbot/yt_dlp_plugins/extractor/discord.py
+++ musicbot/yt_dlp_plugins/extractor/discord.py
@@ -12,6 +12,11 @@ class DiscordAttachmentsIE(InfoExtractor):
         r"/channels/(?P<guild_id>\d+)/(?P<channel_id>\d+)/(?P<message_id>\d+)"
     )
 
+    def _all_attachments(self, data: dict):
+        yield from data["attachments"]
+        for snapshot in data.get("message_snapshots", []):
+            yield from snapshot["message"]["attachments"]
+
     def _real_extract(self, url):
         from musicbot.__main__ import bot
         from musicbot.loader import _loop
@@ -29,6 +34,7 @@ class DiscordAttachmentsIE(InfoExtractor):
             )
         except Exception as e:
             raise DownloadError(str(e)) from e
+
         uploader = resp["author"]["username"]
         entries = [
             {
@@ -37,6 +43,6 @@ class DiscordAttachmentsIE(InfoExtractor):
                 "title": a["filename"],
                 "uploader": uploader,
             }
-            for a in resp["attachments"]
+            for a in self._all_attachments(resp)
         ]
         return {"_type": "playlist", "entries": entries}
