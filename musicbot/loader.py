2b05cbfff5581b3de254790990e14ee25993a9bb
solaluset <60041069+solaluset@users.noreply.github.com>
2026-09-21 15:25:56 +0300

    Return empty list instead of None when searching

--- musicbot/loader.py
+++ musicbot/loader.py
@@ -105,18 +105,18 @@ def _extract_info(url: str, ie: Optional[ExtractorT] = None) -> Optional[dict]:
             return None
 
 
-async def search_youtube(title: str, count: int = 1) -> Optional[List[dict]]:
+async def search_youtube(title: str, count: int = 1) -> List[dict]:
     return await _run_sync(_search_youtube, title, count)
 
 
-def _search_youtube(title: str, count: int = 1) -> Optional[List[dict]]:
+def _search_youtube(title: str, count: int = 1) -> List[dict]:
     """Searches youtube for the video title
     Returns the first results video link"""
 
     r = _extract_info(f"ytsearch{count}:{title}")
 
     if not r:
-        return None
+        return []
 
     return r["entries"]
 
